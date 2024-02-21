0dee6c6e978963643304ad0914aa895e3f85ef78
Shreyas S S <shreyasnarve1611@gmail.com>
2024-02-21 20:15:37 +0530

    add function to read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_contents = read_file(String::from("../text.txt"));
+    
+    println!("{}", file_contents);
+}
+
+fn read_file(path: String) -> String {
+    let file_contents = fs::read_to_string(path)
+    .expect("Failed to read the file");
+
+    file_contents
 }
